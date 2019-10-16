aca98e6b27bfa4c63194da65cee4e3ee85746309
rkiitgn <rkiitgn@gmail.com>
2019-10-16 12:16:22 -0400

    added solution to sherlock anagram problem

---
+++ SherlockAnagrams.py
@@ -0,0 +1,16 @@
+def sherlockAndAnagrams(s):
+    freq = {}
+    for i in range(len(s)):
+        for j in range(i + 1, len(s) + 1):
+            temp_dict = {}
+            for jj in s[i:j]:
+                temp_dict[jj] = temp_dict.get(jj,0)+1
+            if not frozenset(temp_dict.items()) in freq:
+                freq[frozenset(temp_dict.items())] = 0
+            freq[frozenset(temp_dict.items())] += 1
+    res = 0
+    for count in freq.values():
+        res += count*(count-1)/2
+    return res
+
+print(sherlockAndAnagrams("kkkk"))
